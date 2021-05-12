691777ea42c9afa206d58f3212b32f1e1d5558e3
Steven Murdoch <s.murdoch@ucl.ac.uk>
2021-05-12 17:08:07 +0100

    Update for a modern version of bundle

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -3,6 +3,7 @@
 # If there's a Gemfile, then run `bundle install`
 # It's assumed that the Gemfile will install Jekyll too
 if [ -f Gemfile ]; then
+    bundle update --bundler
     bundle install
 else
     # If there's no Gemfile, install Jekyll
